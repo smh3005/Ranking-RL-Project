797aa705f8256735b85aeb2549a7463198fb8530
Seth Hamilton <seth.hamilton801@gmail.com>
2021-04-26 22:08:23 -0500

    mid debugging state

--- ELO.py
+++ ELO.py
@@ -2,13 +2,7 @@ from AbstractAlgo import AbstractAlgo
 from JudgingSimulator import JudgingSimulator
 import numpy as np
 import trueskill
-
-# Simulation Parameters
-# opinion_var = 0.01
-# eval_var    = 0.01
-# num_teams   = 10
-# n_judges  = 1
-# teams_var   = 1
+import random
 
 class ELO(AbstractAlgo):
     def __init__(self):
@@ -16,36 +10,49 @@ class ELO(AbstractAlgo):
 
     def rank_teams(self, n_teams, n_judges, true_q, c, var):
         prev_teams  = [-1 for _ in range(n_judges)]
-        team_scores = [trueskill.Rating(1000) for _ in range(n_teams)]
+        team_scores = [trueskill.Rating(2) for _ in range(n_teams)]
 
         sim = JudgingSimulator(true_q, n_judges, var, var)
 
         num_comparisons = 0
 
         free_teams = set(range(n_teams))
-        steps = 100
+        steps = 50
 
         t = 0
         while t < steps:
             for j in range(n_judges):
+                
                 t1 = prev_teams[j]
-                t2 = free_teams.pop() # TODO: make sure random
-
                 if t1 == -1:
-                    prev_teams[j] = t2
+                    prev_teams[j] = free_teams.pop()
                     continue
+
+                t2 = None
+                closest_dif = 9999 # simulated inf
+                for team in free_teams:
+                    if t2 == None:
+                        t2 = team
+                    elif abs(trueskill.quality_1vs1(team_scores[t2], team_scores[team]) - 0.5) < closest_dif:
+                        closest_dif = abs(trueskill.quality_1vs1(team_scores[t2], team_scores[team]) - 0.5)
+                        t2 = team        
 
                 winner = sim.judge(j, t1, t2)
                 loser = t2 if winner == t1 else t1
 
                 team_scores[t1], team_scores[t2] = trueskill.rate_1vs1(team_scores[winner], 
-                                                                       team_scores[loser])
+                                                                       team_scores[loser], min_delta=0.1)
                 
                 free_teams.add(t1)
                 prev_teams[j] = t2
+                free_teams.remove(t2)
 
             t += 1
 
+        print(team_scores)
+        print(np.argsort(team_scores[::-1]))
+        print(true_q)
+        exit()
         return np.argsort(team_scores[::-1]), t, None
 
     def generate_plots(self):
